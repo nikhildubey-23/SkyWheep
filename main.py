5aab3bd5528b7ca59f86ae36386caaf8796419f3
nikhildubey-23 <ravannick0089@gmail.com>
2024-08-29 14:42:19 +0530

    adding all the important value

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import sql_injection as sqli
+import ssrf_attack as ssrf
+import test_borken_auth as auth
+import test_csp_bypass as csp
+import xss_attack as xss
+import test_missconfiguration as misconfig
+import test_idor as idor
+import test_csrf as csrf
+import test_path_traversal as pt
+
 
 target_url = input("Enter the target url : ")
 
